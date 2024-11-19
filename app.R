42493480bb835aad87c7e373694996f08d1553bb
Andrew Thornton <art27@cantab.net>
2024-11-19 15:43:27 +0000

    Move out the pathwayPlots tab
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- app.R
+++ app.R
@@ -119,53 +119,7 @@ ui <- dashboardPage(
     useShinyjs(),
     tabItems(
       tabItem(tabName = "api", apiTab()),
-      tabItem(tabName = "rxpathwayplots",
-              fluidRow(
-                tabPanel(
-                  "RxPathwayPlots",
-                  column(
-                    width = 3,
-                    radioButtons(
-                      "rxTimesPlotRadio",
-                      "Pathway Plot Type",
-                      c("Treated Plot" = "rxdonePlot",
-                        "Waiting Plot" = "rxwaitPlot")
-                    )
-                  ),
-                  column(
-                    width = 3,
-                    dateInput(
-                      "rxPlotDate1",
-                      "Start Date:",
-                      format = "dd/mm/yyyy",
-                      value = Sys.Date() - 365
-                    ),
-                    dateInput(
-                      "rxPlotDate2",
-                      "End Date:",
-                      format = "dd/mm/yyyy",
-                      value = Sys.Date()
-                    )
-                  ),
-                  column(
-                    width = 6,
-                    checkboxGroupInput(
-                      "organRxPlotCheckbox",
-                      "Organs to Plot",
-                      choices = organFactors,
-                      selected = organFactors
-                    ),
-                  ),
-                  column(
-                    width = 3,
-                    actionButton(inputId = "refreshRxPlot", label = "Refresh Plot")
-                  )
-                )
-              ),
-              fluidRow(box(
-                width = 12,
-                plotlyOutput("plotRxPathway")
-              ))),
+      tabItem(tabName = "rxpathwayplots", pathwayPlotsTab()),
 
       tabItem(tabName = "operatorplots",
               fluidRow(
@@ -557,11 +511,7 @@ server <- function(input, output, session) {
   api <- apiServer(input, output, session)
   tariff <- tariffServer(input, output, session, api)
 
-  finalRxPlotInput <- reactive({
-    switch(input$rxTimesPlotRadio,
-           "rxdonePlot" = rxdonePlot,
-           "rxwaitPlot" = rxwaitPlot)
-  })
+
 
   finalOperatorPlotInput <- reactive({
      operatorPlot
@@ -633,25 +583,7 @@ server <- function(input, output, session) {
     #})
   })
 
-  # Plot the Rx pathway plot using the date range and organ filters
-  # Note plotly vs. plot gives you the tool tip text
-  output$plotRxPathway <- renderPlotly({
-    if (!is.list(finalRxTableDataInput()))
-    {
-      p <- plot.new()
-    }
-    else
-    {
-      p <- finalRxPlotInput() +
-        scale_x_date(limits = as.Date(c(
-          input$rxPlotDate1, input$rxPlotDate2
-        ), format = "%d/%m/%Y")) +
-        theme(legend.position = "bottom")
-      p <- p %+% subset(finalRxDataInput(), Organs %in% input$organRxPlotCheckbox)
-    }
-    plots$activePlot <- p
-    plots$activePlot
-  })
+
 
   # Note plotly vs. plot gives you the tool tip text
   output$plotOperators <- renderPlotly({
