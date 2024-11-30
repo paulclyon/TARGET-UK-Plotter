d716452c9bed91d482697326d44f7cf17c80afd0
Andrew Thornton <art27@cantab.net>
2024-11-30 18:20:06 +0000

    Add resize detection to the pathway pie
    This one was a bit more involved due to having to fix a few issues.
    I was able to simplify the code quite a bit.
    
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- lib/ui/pathwayPie.R
+++ lib/ui/pathwayPie.R
@@ -1,87 +1,104 @@
 pathwayPieTab <- function() {
-  list(fluidRow(
-    tabPanel(
-      "RxPathwayPies",
-      column(
-        width = 3,
-        radioButtons(
-          "rxTimesPieRadio",
-          "Pathway Pie Chart Type",
-          c("Treated Pie"  = "rxdonePie",
-            "Waiting Pie"  = "rxwaitPie")
-        )
-      ),
-      column(
-        width = 3,
-        dateInput(
-          "rxPieDate1",
-          "Start Date:",
-          format = "dd/mm/yyyy",
-          value = Sys.Date() - 365
+  list(
+    fluidRow(
+      tabPanel(
+        "RxPathwayPies",
+        column(
+          width = 3,
+          radioButtons(
+            "rxTimesPieRadio",
+            "Pathway Pie Chart Type",
+            c(
+              "Treated Pie" = "treatedPie",
+              "Waiting Pie" = "waitingPie"
+            )
+          )
         ),
-        dateInput(
-          "rxPieDate2",
-          "End Date:",
-          format = "dd/mm/yyyy",
-          value = Sys.Date()
-        )
-      ),
-      column(
-        width = 3,
-        checkboxGroupInput(
-          "organPieCheckbox",
-          "Organs to Chart",
-          choices = organFactors,
-          selected = organFactors
+        column(
+          width = 3,
+          dateInput(
+            "pieStartDate",
+            "Start Date:",
+            format = "dd/mm/yyyy",
+            value = Sys.Date() - 365
+          ),
+          dateInput(
+            "pieEndDate",
+            "End Date:",
+            format = "dd/mm/yyyy",
+            value = Sys.Date()
+          )
         ),
-      ),
-      column(
-        width = 3,
-        actionButton(inputId = "refreshRxPie", label = "Refresh Pie Chart")
+        column(
+          width = 3,
+          checkboxGroupInput(
+            "pieSelectedOrgans",
+            "Organs to Chart",
+            choices = organFactors,
+            selected = organFactors
+          ),
+        ),
+        column(
+          width = 3,
+          actionButton(inputId = "refreshRxPie", label = "Refresh Pie Chart")
+        )
       )
-    )
-  ),
-  fluidRow(box(
-    width = 12,
-    plotOutput("pieRxPathway")
-  )))
+    ),
+    fluidRow(box(
+      width = 12,
+      plotOutput("pieRxPathway")
+    )),
+    detectHeightJS("rxpathwaypies", "pieRxPathway")
+  )
 }
 
 pathwayPieServer <- function(input, output, session, api, plots) {
-  finalRxPieInput <- reactive({
+  selectedPiePlot <- reactive({
     switch(input$rxTimesPieRadio,
-           "rxdonePie" = rxdonePie,
-           "rxwaitPie" = rxwaitPie)
+      "treatedPie" = treatedPie(),
+      "waitingPie" = waitingPie()
+    )
   })
 
-  finalRxTableDataInput <- reactive({
-    switch(input$rxTimesTableRadio,
-           "rxdoneTable" = rxDoneData,
-           "rxwaitTable" = rxWaitData)
+  selectedData <- reactive({
+    switch(input$rxTimesPieRadio,
+      "treatedPie" = rxDoneData,
+      "waitingPie" = rxWaitData
+    )
   })
+
+  treatedPie <- reactive(
+    makeRxDonePie(
+      input$pieStartDate,
+      input$pieEndDate,
+      input$pieSelectedOrgans
+    )
+  )
+
+  waitingPie <- reactive(
+    makeRxWaitPie(
+      input$pieStartDate,
+      input$pieEndDate,
+      input$pieSelectedOrgans
+    )
+  )
+
+  height <- reactive(detectedHeight(input, "pieRxPathway"))
 
   output$pieRxPathway <- renderPlot(
     {
-      if (!is.list(finalRxTableDataInput()))
-      {
-        p <- plot.new()
+      if (!is.list(selectedData())) {
+        return(plot.new())
       }
-      else
-      {
-        makeRxDonePie(input$rxPieDate1,
-                      input$rxPieDate2,
-                      input$organPieCheckbox)
-        makeRxWaitPie(input$rxPieDate1,
-                      input$rxPieDate2,
-                      input$organPieCheckbox)
-        p <- finalRxPieInput()
-      }
-      plots$activePlot <- p
+
+      plots$activePlot <- selectedPiePlot()
       plots$activePlot
-    })
+    },
+    height = height
+  )
 
   observe({
-    updateCheckboxGroupInput(session, "organPieCheckbox", "Organs to Chart",
+    updateCheckboxGroupInput(session, "pieSelectedOrgans", "Organs to Chart",
       choices = api$organFactors,
       selected = api$organFactors
     )
