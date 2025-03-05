5465e364216d8214aacd93bae5b86578897251a8
Andrew Thornton <art27@cantab.net>
2025-03-05 12:07:08 +0000

    Add dashboard plots
    Signed-off-by: Andrew Thornton <art27@cantab.net>

---
+++ lib/ui/referralDashboardPlot.R
@@ -0,0 +1,122 @@
+referralDashboardPlotTab <- function() {
+    list(
+        fluidRow(
+            tabPanel(
+                "ReferralDashboard",
+                column(
+                    width = 3,
+                    dateInput(
+                        "referralDashboardPlotStart",
+                        "Start Date:",
+                        format = "dd/mm/yyyy",
+                        value = Sys.Date() - 365
+                    ),
+                    dateInput(
+                        "referralDashboardPlotEnd",
+                        "End Date:",
+                        format = "dd/mm/yyyy",
+                        value = Sys.Date()
+                    ),
+                ),
+                column(
+                    width = 3,
+                    checkboxGroupInput(
+                        "referralDashboardPlotOrganCheckbox",
+                        "Organs to Plot",
+                        choices = organFactors,
+                        selected = organFactors
+                    ),
+                ),
+                column(
+                    width = 3,
+                    radioButtons(
+                        "referralDashboardPlotDurationRadio",
+                        "Duration of Plot",
+                        c(
+                            "Weekly" = "week",
+                            "2 Weekly" = "2 weeks",
+                            "Monthly" = "month",
+                            "Quarterly" = "quarter",
+                            "Yearly" = "year"
+                        ),
+                        selected = "month"
+                    )
+                ),
+                column(
+                    width = 3,
+                    actionButton(inputId = "referralDashboardPlotRefresh", label = "Refresh Plot")
+                )
+            )
+        ),
+        fluidRow(
+            box(
+                width = 4,
+                plotlyOutput("plotReferralDashboardRefToDTT")
+            ),
+            box(
+                width = 4,
+                plotlyOutput("plotReferralDashboardDTTToRx")
+            ),
+            box(
+                width = 4,
+                plotlyOutput("plotReferralDashboardRefToRx")
+            ),
+        ),
+        detectHeightJS("referralDashboard", "plotReferralDashboardRefToDTT"),
+        detectHeightJS("referralDashboard", "plotReferralDashboardDTTToRx"),
+        detectHeightJS("referralDashboard", "plotReferralDashboardRefToRx")
+    )
+}
+
+referralDashboardPlotServer <- function(input, output, session, api, plots) {
+    observe({
+        updateCheckboxGroupInput(session, "referralDashboardPlotOrganCheckbox", "Organs to Plot",
+            choices = api$organFactors,
+            selected = api$organFactors
+        )
+    })
+
+    referralTimes <- reactive(processReferralTimesPerPeriod(
+        rxDoneData %>% filter(Organs %in% input$referralDashboardPlotOrganCheckbox),
+        rxWaitData %>% filter(Organs %in% input$referralDashboardPlotOrganCheckbox),
+        input$referralDashboardPlotStart,
+        input$referralDashboardPlotEnd,
+        input$referralDashboardPlotDurationRadio
+    ))
+
+
+    output$plotReferralDashboardRefToDTT <- renderPlotly({
+        p <- refToDTTMeanPlot(referralTimes(), input$referralDashboardPlotDurationRadio)
+
+        height <- detectedHeight(input, "plotReferralDashboardRefToDTT")
+
+        p <- ggplotly(p, height = height)
+        plots$activePlot <- p
+        p
+    })
+
+    output$plotReferralDashboardRefToRx <- renderPlotly({
+        p <- refToRxMeanPlot(referralTimes(), input$referralDashboardPlotDurationRadio)
+
+        height <- detectedHeight(input, "plotReferralDashboardRefToRx")
+
+        p <- ggplotly(p, height = height)
+        plots$activePlot <- p
+        p
+    })
+
+    output$plotReferralDashboardDTTToRx <- renderPlotly({
+        p <- dttToRxMeanPlot(referralTimes(), input$referralDashboardPlotDurationRadio)
+
+        height <- detectedHeight(input, "plotReferralDashboardDTTToRx")
+
+        p <- ggplotly(p, height = height)
+        plots$activePlot <- p
+        p
+    })
+
+
+    observeEvent(input$referralDashboardPlotRefresh, {
+        plots$activePlot <- ggplot()
+    })
+}
