cf7054f2ee5a63df1bc3c84aa8bfe51d157809bf
Andrew Thornton <art27@cantab.net>
2025-03-07 20:54:45 +0000

    Fix a few issues, axis labels and limits
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- lib/plots/referral_dashboard_plot.R
+++ lib/plots/referral_dashboard_plot.R
@@ -37,10 +37,10 @@ processReferralTimesPerPeriod <- function(doneData, waitingData, start, end, ran
         # Only include patients referred before the end date
         filter(RefDate < as.Date(range_end(period)) &
           # & either DTTDate after the start of the period
-          DTTDate >= as.Date(range_start(period)) |
-          # or DTTDate is missing and RxDate is missing or after the start of the period
-          (is.na(DTTDate) &
-            (is.na(RxDate) | RxDate >= as.Date(range_start(period))))) |>
+          (DTTDate >= as.Date(range_start(period)) |
+            # or DTTDate is missing and RxDate is missing or after the start of the period
+            (is.na(DTTDate) &
+              (is.na(RxDate) | RxDate >= as.Date(range_start(period)))))) |>
         mutate(
           DTTDateFixed = pmin(DTTDate, as.Date(range_end(period)), RxDate, na.rm = T),
           RefToDTT = DTTDateFixed - RefDate,
@@ -55,6 +55,9 @@ processReferralTimesPerPeriod <- function(doneData, waitingData, start, end, ran
           MeanRefToDTT = mean(RefToDTTNoStopped, na.rm = TRUE),
           RefToDTTStopped = sum(DTTStopped, na.rm = TRUE),
           RefToDTTNotStopped = sum(RefToDTTClockStopped >= 0, na.rm = TRUE),
+          "RefToDTT ≤10 days" = sum(RefToDTTNoStopped <= 10, na.rm = TRUE),
+          "RefToDTT ≤21 days" = sum(RefToDTTNoStopped > 10 & RefToDTTNoStopped <= 21, na.rm = TRUE),
+          "RefToDTT >21 days" = sum(RefToDTTNoStopped > 21, na.rm = TRUE)
         )
     },
     DTTToRx = \(x, period) {
@@ -86,6 +89,10 @@ processReferralTimesPerPeriod <- function(doneData, waitingData, start, end, ran
           MeanDTTToRx = mean(DTTToRxNoStopped, na.rm = TRUE),
           DTTToRxStopped = sum(RxStopped, na.rm = TRUE),
           DTTToRxNotStopped = sum(DTTToRxClockStopped >= 0, na.rm = TRUE),
+          "DTTToRx ≤31 days" = sum(DTTToRxNoStopped <= 31, na.rm = TRUE),
+          "DTTToRx ≤45 days" = sum(DTTToRxNoStopped > 31 & DTTToRxNoStopped <= 45, na.rm = TRUE),
+          "DTTToRx ≤60 days" = sum(DTTToRxNoStopped > 45 & DTTToRxNoStopped <= 60, na.rm = TRUE),
+          "DTTToRx >60 days" = sum(DTTToRxNoStopped > 60, na.rm = TRUE)
         )
     },
     RefToRx = \(x, period) {
@@ -108,6 +115,8 @@ processReferralTimesPerPeriod <- function(doneData, waitingData, start, end, ran
           MeanRefToRx = mean(RefToRxNoStopped, na.rm = TRUE),
           RefToRxStopped = sum(RxStopped, na.rm = TRUE),
           RefToRxNotStopped = sum(RefToRxClockStopped >= 0, na.rm = TRUE),
+          "RefToRx ≤90 days" = sum(RefToRxNoStopped <= 90, na.rm = TRUE),
+          "RefToRx >90 days" = sum(RefToRxNoStopped > 90, na.rm = TRUE)
         )
     }
   )
@@ -127,26 +136,31 @@ processReferralTimesPerPeriod <- function(doneData, waitingData, start, end, ran
 }
 
 refToDTTMeanPlot <- function(referralTimes, range_by = "Monthly") {
+  maxY <- max(25, ceiling((max(referralTimes$MeanRefToDTT) + 1) / 5) * 5)
+
   referralTimes |>
-    mutate(BreachStatus = if_else(MeanRefToDTT > 10, if_else(MeanRefToDTT > 31, ">31 days", ">10 days"), "<10 days")) |>
+    mutate(BreachStatus = if_else(MeanRefToDTT > 10, if_else(MeanRefToDTT > 21, ">21 days", ">10 days"), "<10 days")) |>
     ggplot() +
     geom_line(aes(x = PeriodStart, y = MeanRefToDTT, color = "grey")) +
     geom_point(aes(x = PeriodStart, y = MeanRefToDTT, color = BreachStatus)) +
     geom_hline(yintercept = 10, linetype = "dashed", color = "orange") +
-    geom_hline(yintercept = 31, linetype = "dashed", color = "red") +
+    geom_hline(yintercept = 21, linetype = "dashed", color = "red") +
     scale_color_manual(
       name = "Breach Status",
-      values = c("<10 days" = "grey", ">10 days" = "orange", ">31 days" = "red")
+      values = c("≤10 days" = "grey", "≤21 days" = "orange", ">21 days" = "red")
     ) +
     guides(color = "none") +
     scale_x_date(date_breaks = "month", date_labels = "%b-%y") +
+    scale_y_continuous(breaks = seq(0, maxY, by = 5), limits = c(0, maxY)) +
     labs(
       title =
         paste0(
           stringr::str_to_title(range_by),
           " Referral to DTT Times (Generated ",
           format(Sys.time(), "%a %b %d %Y %X"), ")"
-        )
+        ),
+      x = "",
+      y = "Number of Days"
     ) +
     theme(plot.title = element_text(size = 10)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.spacing.y = unit(0.1, "cm"))
@@ -154,17 +168,23 @@ refToDTTMeanPlot <- function(referralTimes, range_by = "Monthly") {
 
 refToDTTCountPlot <- function(referralTimes, range_by = "Monthly") {
   referralTimes |>
+    select(
+      PeriodStart, `RefToDTT ≤10 days`,
+      `RefToDTT ≤21 days`, `RefToDTT >21 days`
+    ) |>
+    pivot_longer(cols = -PeriodStart, names_to = "Breach Status", values_to = "Count") |>
+    mutate(
+      `Breach Status` = factor(
+        `Breach Status`,
+        levels = c("RefToDTT >21 days", "RefToDTT ≤21 days", "RefToDTT ≤10 days")
+      )
+    ) |>
     ggplot() +
-    geom_bar(aes(x = PeriodStart, y = RefToDTTStopped, fill = "Stopped"),
+    geom_bar(aes(x = PeriodStart, y = Count, fill = `Breach Status`),
       stat = "identity",
       alpha = 0.75,
       just = 0,
-      position = "dodge2"
-    ) +
-    geom_bar(aes(x = PeriodStart, y = RefToDTTNotStopped, fill = "Not Stopped"),
-      stat = "identity", alpha = 0.75,
-      just = 0,
-      position = "dodge2"
+      position = "stack"
     ) +
     scale_x_date(date_breaks = "month", date_labels = "%b-%y") +
     labs(
@@ -173,33 +193,46 @@ refToDTTCountPlot <- function(referralTimes, range_by = "Monthly") {
           stringr::str_to_title(range_by),
           " Referral to DTT Counts (Generated ",
           format(Sys.time(), "%a %b %d %Y %X"), ")"
-        )
+        ),
+      x = "",
+      y = "Number of Patients"
     ) +
     theme(plot.title = element_text(size = 10)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.spacing.y = unit(0.1, "cm"))
 }
 
 dttToRxMeanPlot <- function(referralTimes, range_by = "Monthly") {
+  maxY <- max(65, ceiling((max(referralTimes$MeanDTTToRx) + 1) / 5) * 5)
+
   referralTimes |>
-    mutate(BreachStatus = if_else(MeanDTTToRx > 31, if_else(MeanDTTToRx > 45, ">45 days", ">31 days"), "<31 days")) |>
+    mutate(BreachStatus = case_when(
+      MeanDTTToRx > 60 ~ ">60 days",
+      MeanDTTToRx > 45 ~ "≤60 days",
+      MeanDTTToRx > 31 ~ "≤45 days",
+      TRUE ~ "≤31 days"
+    )) |>
     ggplot() +
     geom_line(aes(x = PeriodStart, y = MeanDTTToRx, color = "grey")) +
     geom_point(aes(x = PeriodStart, y = MeanDTTToRx, color = BreachStatus)) +
     geom_hline(yintercept = 31, linetype = "dashed", color = "orange") +
     geom_hline(yintercept = 45, linetype = "dashed", color = "red") +
+    geom_hline(yintercept = 60, linetype = "dashed", color = "darkred") +
     scale_color_manual(
       name = "Breach Status",
-      values = c("<31 days" = "grey", ">31 days" = "orange", ">45 days" = "red")
+      values = c("≤31 days" = "grey", "≤45 days" = "orange", "≤60 days" = "red", ">60 days" = "darkred")
     ) +
     guides(color = "none") +
     scale_x_date(date_breaks = "month", date_labels = "%b-%y") +
+    scale_y_continuous(breaks = seq(0, maxY, by = 5), limits = c(0, maxY)) +
     labs(
       title =
         paste0(
           stringr::str_to_title(range_by),
           " DTT to Rx Times (Generated ",
           format(Sys.time(), "%a %b %d %Y %X"), ")"
-        )
+        ),
+      x = "",
+      y = "Number of Days"
     ) +
     theme(plot.title = element_text(size = 10)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.spacing.y = unit(0.1, "cm"))
@@ -207,32 +240,47 @@ dttToRxMeanPlot <- function(referralTimes, range_by = "Monthly") {
 
 dttToRxCountPlot <- function(referralTimes, range_by = "Monthly") {
   referralTimes |>
+    select(
+      PeriodStart, `DTTToRx ≤31 days`,
+      `DTTToRx ≤45 days`, `DTTToRx ≤60 days`, `DTTToRx >60 days`
+    ) |>
+    pivot_longer(cols = -PeriodStart, names_to = "Breach Status", values_to = "Count") |>
+    mutate(
+      `Breach Status` = factor(
+        `Breach Status`,
+        levels = c(
+          "DTTToRx >60 days",
+          "DTTToRx ≤60 days",
+          "DTTToRx ≤45 days",
+          "DTTToRx ≤31 days"
+        )
+      )
+    ) |>
     ggplot() +
-    geom_bar(aes(x = PeriodStart, y = RefToDTTStopped, fill = "Stopped"),
+    geom_bar(aes(x = PeriodStart, y = Count, fill = `Breach Status`),
       stat = "identity",
       alpha = 0.75,
       just = 0,
-      position = "dodge2"
-    ) +
-    geom_bar(aes(x = PeriodStart, y = RefToDTTNotStopped, fill = "Not Stopped"),
-      stat = "identity", alpha = 0.75,
-      just = 0,
-      position = "dodge2"
+      position = "stack"
     ) +
     scale_x_date(date_breaks = "month", date_labels = "%b-%y") +
     labs(
       title =
         paste0(
           stringr::str_to_title(range_by),
-          " Referral to DTT Counts (Generated ",
+          " DTT to Rx Counts (Generated ",
           format(Sys.time(), "%a %b %d %Y %X"), ")"
-        )
+        ),
+      x = "",
+      y = "Number of Patients"
     ) +
     theme(plot.title = element_text(size = 10)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.spacing.y = unit(0.1, "cm"))
 }
 
 refToRxMeanPlot <- function(referralTimes, range_by = "Monthly") {
+  maxY <- max(90, ceiling((max(referralTimes$MeanRefToRx) + 1) / 5) * 5)
+
   referralTimes |>
     mutate(BreachStatus = if_else(MeanRefToRx > 90, ">90 days", "<90 days")) |>
     ggplot() +
@@ -245,13 +293,16 @@ refToRxMeanPlot <- function(referralTimes, range_by = "Monthly") {
     ) +
     guides(color = "none") +
     scale_x_date(date_breaks = "month", date_labels = "%b-%y") +
+    scale_y_continuous(breaks = seq(0, maxY, by = 5), limits = c(0, maxY)) +
     labs(
       title =
         paste0(
           stringr::str_to_title(range_by),
           " Referral to Rx Times (Generated ",
           format(Sys.time(), "%a %b %d %Y %X"), ")"
-        )
+        ),
+      x = "",
+      y = "Number of Days"
     ) +
     theme(plot.title = element_text(size = 10)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.spacing.y = unit(0.1, "cm"))
@@ -259,26 +310,28 @@ refToRxMeanPlot <- function(referralTimes, range_by = "Monthly") {
 
 refToRxCountPlot <- function(referralTimes, range_by = "Monthly") {
   referralTimes |>
+    select(PeriodStart, `RefToRx ≤90 days`, `RefToRx >90 days`) |>
+    pivot_longer(cols = -PeriodStart, names_to = "Breach Status", values_to = "Count") |>
+    mutate(
+      `Breach Status` = factor(`Breach Status`, levels = c("RefToRx >90 days", "RefToRx ≤90 days"))
+    ) |>
     ggplot() +
-    geom_bar(aes(x = PeriodStart, y = RefToDTTStopped, fill = "Stopped"),
+    geom_bar(aes(x = PeriodStart, y = Count, fill = `Breach Status`),
       stat = "identity",
       alpha = 0.75,
       just = 0,
-      position = "dodge2"
-    ) +
-    geom_bar(aes(x = PeriodStart, y = RefToDTTNotStopped, fill = "Not Stopped"),
-      stat = "identity", alpha = 0.75,
-      just = 0,
-      position = "dodge2"
+      position = "stack"
     ) +
     scale_x_date(date_breaks = "month", date_labels = "%b-%y") +
     labs(
       title =
         paste0(
           stringr::str_to_title(range_by),
-          " Referral to DTT Counts (Generated ",
+          " Referral to Rx Counts (Generated ",
           format(Sys.time(), "%a %b %d %Y %X"), ")"
-        )
+        ),
+      x = "",
+      y = "Number of Patients"
     ) +
     theme(plot.title = element_text(size = 10)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.spacing.y = unit(0.1, "cm"))
