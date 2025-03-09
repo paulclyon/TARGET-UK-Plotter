9e7a8689874b86a2b162868dc7ba9eba98d2da8a
Andrew Thornton <art27@cantab.net>
2025-03-09 11:21:10 +0000

    formatting and styling fixes
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- lib/ui/audit.R
+++ lib/ui/audit.R
@@ -29,7 +29,7 @@ auditTab <- function() {
       column(
         width = 3,
         actionButton(inputId = "buttonRunAuditReport", label = "Run Audit Report"),
-        actionButton(inputId = "buttonAuditToPDF",     label = "Report to PDF")
+        actionButton(inputId = "buttonAuditToPDF", label = "Report to PDF")
       )
     )),
     wellPanel(
@@ -41,39 +41,38 @@ auditTab <- function() {
   )
 }
 
-auditServer <- function(input, output, session, api, plots)
-{
+auditServer <- function(input, output, session, api, plots) {
   rmdAuditFile <- c(Sys.getenv("AUDIT_PATHWAY_RMD"))
   mdAuditFile <- c(Sys.getenv("AUDIT_PATHWAY_MD"))
-  
+
   finalRefAuditInput <- reactive({
     tryCatch(
-    {
-      # Clear the old HTML file if it exists...
-      if (file.exists(mdAuditFile))
       {
-        showNotification(paste("Cleaning out the old HTML file '",mdAuditFile,"'",sep=""))
-        file.remove(mdAuditFile)
+        # Clear the old HTML file if it exists...
+        if (file.exists(mdAuditFile)) {
+          showNotification(paste("Cleaning out the old HTML file '", mdAuditFile, "'", sep = ""))
+          file.remove(mdAuditFile)
+        }
+
+        # This does the knitting bit ready to make the HTML by running the knit function
+        # This makes the MD file which is basically just HTML in a file, which we then return as includeMarkdown()
+        sapply(rmdAuditFile, knit, quiet = T)
+        htmltools::includeMarkdown(mdAuditFile)
+      },
+      error = function(errorMessage) {
+        logger(conditionMessage(errorMessage), T)
+        showNotification(conditionMessage(errorMessage))
+        # Choose a return value in case of error
+        NA
       }
-      
-      # This does the knitting bit ready to make the HTML by running the knit function
-      # This makes the MD file which is basically just HTML in a file, which we then return as includeMarkdown()
-      sapply(rmdAuditFile, knit, quiet = T)
-      htmltools::includeMarkdown(mdAuditFile)
-    },
-    error = function(errorMessage) {
-      logger(conditionMessage(errorMessage),T)
-      showNotification(conditionMessage(errorMessage))
-      # Choose a return value in case of error
-      NA
-    })
+    )
   })
 
   observe({
-      updateCheckboxGroupInput(session, "organAuditCheckbox", "Organs to Chart",
-        choices = api$organFactors,
-        selected = api$organFactors
-      )
+    updateCheckboxGroupInput(session, "organAuditCheckbox", "Organs to Chart",
+      choices = api$organFactors,
+      selected = api$organFactors
+    )
   })
 
   observeEvent(input$buttonRunAuditReport, {
@@ -81,30 +80,39 @@ auditServer <- function(input, output, session, api, plots)
 
     # This is a bit of an ugly hack to allow markdown to see global vars but it doesn't appear to work FIXME
     audit_start_date <<- input$auditDate1
-    audit_end_date   <<- input$auditDate2
-    audit_organs     <<- input$organAuditCheckbox
+    audit_end_date <<- input$auditDate2
+    audit_organs <<- input$organAuditCheckbox
 
     # Elegant way to regenerate the HTML such that is does allow dynamic refresh
     output$summaryRefAudit <- renderUI({
-      shinyCatch({
-        message(paste("Generating the audit from file '",Sys.getenv("AUDIT_PATHWAY_RMD"),"' to file '",Sys.getenv("AUDIT_PATHWAY_MD"),"'",sep=""))
-      }, prefix = "")
+      shinyCatch(
+        {
+          message(paste(
+            "Generating the audit from file '", Sys.getenv("AUDIT_PATHWAY_RMD"),
+            "' to file '", Sys.getenv("AUDIT_PATHWAY_MD"), "'",
+            sep = ""
+          ))
+        },
+        prefix = ""
+      )
       tryCatch(
-      {
-        # Could also set knit_root_dir parameter but it is probably more trouble than it is worth
-        # The default is the directory of the input file .Rmd which works fine, and if specified, is relative to that dir
-        includeHTML(rmarkdown::render(rmdAuditFile, output_dir=Sys.getenv("REPORT_OUTPUT_DIR")))
-      },
-      error = function(errorMessage) {
-        logger(conditionMessage(errorMessage),T)
-        showNotification(conditionMessage(errorMessage))
-        errorMessage # Return value in case of error
-      })
+        {
+          # Could also set knit_root_dir parameter but it is probably more trouble than it is worth
+          # The default is the directory of the input file .Rmd which works fine, and if specified,
+          # is relative to that dir
+          includeHTML(rmarkdown::render(rmdAuditFile, output_dir = Sys.getenv("REPORT_OUTPUT_DIR")))
+        },
+        error = function(errorMessage) {
+          logger(conditionMessage(errorMessage), T)
+          showNotification(conditionMessage(errorMessage))
+          errorMessage # Return value in case of error
+        }
+      )
     })
-    
+
     # This is another way to do the magic - embed the output into the observe event to allow refresh!
     # But doesn't allow multiple refreshes, see the more simple elegant solution I used
-    #output$summaryRefAudit2 <- renderPrint({
+    # output$summaryRefAudit2 <- renderPrint({
     #  if (api$connected == T && api$loaded == T)
     #  {
     #    showNotification(paste("Generating the audit from file '",Sys.getenv("AUDIT_PATHWAY_RMD"),"'",sep=""))
@@ -117,56 +125,82 @@ auditServer <- function(input, output, session, api, plots)
     #      "There is no study data loaded at present - cannot run the audit"
     #  }
     #  thisHTML
-    #})
+    # })
   })
-  
+
   observeEvent(input$buttonAuditToPDF, {
-    if (isDocker == T)
-    {
-      shinyCatch({
-        message("Sorry running in a Docker via Web interface therefore audit PDF export function not available...")
-      }, prefix = '')
-    }
-    else
-    {
+    if (isDocker == T) {
+      shinyCatch(
+        {
+          message("Sorry running in a Docker via Web interface therefore audit PDF export function not available...")
+        },
+        prefix = ""
+      )
+    } else {
       exportFile <- NA
-      shinyCatch({
-        message("If this is a secure computer (patient IDs included), choose a PDF file to export in a directory with write access...")
-      }, prefix = "")
+      shinyCatch(
+        {
+          message("If this is a secure computer (patient IDs included), choose a PDF file to export in a directory with write access...")
+        },
+        prefix = ""
+      )
       outputPDF <- "targetuk_waiting_time_audit_report.pdf"
-      if (dir.exists("reports")) outputPDF <- paste0("reports/",outputPDF)
-      result = tryCatch({ exportFile <- svDialogs::dlg_save(title = "Save R script to", default=outputPDF) }, error = function(err) { logger(err,F) })
-      if (length(exportFile$res >0) && !is.na(exportFile$res) && exportFile$res != "")
-      {
+      if (dir.exists("reports")) outputPDF <- paste0("reports/", outputPDF)
+      result <- tryCatch(
+        {
+          exportFile <- svDialogs::dlg_save(title = "Save R script to", default = outputPDF)
+        },
+        error = function(err) {
+          logger(err, F)
+        }
+      )
+      if (length(exportFile$res > 0) && !is.na(exportFile$res) && exportFile$res != "") {
         if (!endsWith(exportFile$res, ".pdf")) {
           exportFile <- paste(exportFile$res, ".pdf", sep = "")
         }
-        shinyCatch({
-          message(paste("Attempting to export PDF to file (make take some time if first PDF export)...", exportFile$res))
-        }, prefix = "")
-        if (file.access(exportFile$res, mode=2) != -1)  # 0 success i.e. writable (mode 2) / -1 failure
-        {
+
+        shinyCatch(
+          {
+            message(paste(
+              "Attempting to export PDF to file (make take some time if first PDF export)...", exportFile$res
+            ))
+          },
+          prefix = ""
+        )
+
+        if (file.access(exportFile$res, mode = 2) != -1) { # 0 success i.e. writable (mode 2) / -1 failure
           # Could also set knit_root_dir parameter but it is probably more trouble than it is worth
-          # The default is the directory of the input file .Rmd which works fine, and if specified, is relative to that dir
-          rmarkdown::render(rmdAuditFile,"pdf_document", output_dir=Sys.getenv("REPORT_OUTPUT_DIR"), output_file=exportFile$res, quiet=FALSE)
-          shinyCatch({
-            message(paste("Exported PDF to file", exportFile$res))
-          }, prefix = "")          
+          # The default is the directory of the input file .Rmd which works fine, and if specified,
+          # is relative to that dir
+          rmarkdown::render(
+            rmdAuditFile,
+            "pdf_document",
+            output_dir = Sys.getenv("REPORT_OUTPUT_DIR"),
+            output_file = exportFile$res,
+            quiet = FALSE
+          )
+          shinyCatch(
+            {
+              message(paste("Exported PDF to file", exportFile$res))
+            },
+            prefix = ""
+          )
+        } else {
+          shinyCatch(
+            {
+              message(paste("Cannot write access file '", exportFile$res, "' for writing to...", sep = ""))
+            },
+            prefix = ""
+          )
         }
-        else
-        {
-          shinyCatch({
-            message(paste("Cannot write access file '", exportFile$res,"' for writing to...",sep=""))
-          }, prefix = "")   
-        }
-      }
-      else
-      {
-        shinyCatch({
-          message(paste("No file selected to export to, no PDF export performed"))
-        }, prefix = "")
+      } else {
+        shinyCatch(
+          {
+            message(paste("No file selected to export to, no PDF export performed"))
+          },
+          prefix = ""
+        )
       }
     }
   })
-  
 }
